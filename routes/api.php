f0cf159f33f42b9de8e195ddd222e0d3a6093162
Gerardo Belot <gerardo.belot@gmail.com>
2019-11-18 14:06:17 -0600

    WIP fixes varios

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,11 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->get('/v1/user', function (Request $request) {
+    $id = $request->user()->id;
+    $user = \App\User::where('id', $id)->with('roles')->first();
+    return $user;
+
 });
 
 Route::post('v1/login', 'Auth\AuthController@login');
@@ -23,3 +26,5 @@ Route::post('/v1/register', 'Auth\AuthController@register');
 Route::middleware('auth:api')->get('/v1', function (){
    return response()->json(['nombre' => 'hola'], 200);
 });
+
+Route::resource('v1/users', 'UserController');
