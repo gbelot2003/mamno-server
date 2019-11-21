bd4ffe516ec6ab3cd583e14dc2ba557138549117
Gerardo Belot <gerardo.belot@gmail.com>
2019-11-21 14:17:53 -0600

    Agregando info de grupos

--- app/Grupo.php
+++ app/Grupo.php
@@ -7,8 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Grupo extends Model
 {
 
+    /**
+     * @var array
+     */
     protected $fillable = ['name'];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function users()
     {
         return $this->hasMany(User::class, 'grupo_id', 'id');
--- database/migrations/2019_11_21_195134_create_grupos_table.php
+++ database/migrations/2019_11_21_195134_create_grupos_table.php
@@ -15,6 +15,7 @@ class CreateGruposTable extends Migration
     {
         Schema::create('grupos', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
             $table->timestamps();
         });
     }
