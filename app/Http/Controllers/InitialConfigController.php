e0560b34fad21fb73e00e0bd6220f85dc95b2815
Gerardo Belot <gerardo.belot@gmail.com>
2019-11-26 17:01:36 -0600

    WIP actualizando usuarios con bandera de nuevo

--- app/Http/Controllers/InitialConfigController.php
+++ app/Http/Controllers/InitialConfigController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class InitialConfigController extends Controller
 {
-    //
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     * api/v1/configuraciones/nuevos
+     */
+    public function newUsers()
+    {
+        $users = User::where('nuevo', true)->get();
+        return response()->json($users, 200);
+    }
 }
