5349b9cd2512a209de1fcf662960ba154c6cfab4
Gerardo Belot <gerardo.belot@gmail.com>
2019-11-26 21:11:34 -0600

    Cambios en rutas

--- routes/api.php
+++ routes/api.php
@@ -14,12 +14,6 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::middleware('auth:api')->get('/v1/user', function (Request $request) {
-    $id = auth('api')->user()->getAuthIdentifier();
-    $user = \App\User::where('id', $id)->with('roles')->first();
-    return $user;
-});
-
 Route::post('v1/login', 'Auth\AuthController@login');
 Route::post('v1/register', 'Auth\AuthController@register');
 Route::middleware('auth:api')->get('v1/logout', 'Auth\AuthController@logout');
@@ -28,13 +22,21 @@ Route::middleware('auth:api')->get('/v1', function (){
    return response()->json(['nombre' => 'hola'], 200);
 });
 
-Route::middleware('auth:api')->resource('v1/users', 'UserController');
-Route::middleware('auth:api')->post('v1/configuraciones/{id}', 'UserController@configuraciones');
+Route::middleware('auth:api')->get('/v1/user', function (Request $request) {
+    $id = auth('api')->user()->getAuthIdentifier();
+    $user = \App\User::where('id', $id)->with('roles')->first();
+    return $user;
+});
 
 Route::get('v1/departamentos', 'DepartamentosController@index');
 Route::get('v1/municipios/{dep}', 'MunicipiosController@index');
 Route::get('v1/roles', 'RolesController@index');
 Route::get('v1/grupos', 'GrupoController@index');
 
-Route::get('v1/configuraciones/nuevos', 'InitialConfigController@newUsers');
-Route::get('v1/configuraciones/changeStatus/{id}', 'InitialConfigController@changeStatus');
+Route::middleware('auth:api')->resource('v1/users', 'UserController');
+Route::middleware('auth:api')->post('v1/configuraciones/{id}', 'UserController@configuraciones');
+
+
+
+Route::middleware('auth:api')->get('v1/configuraciones/nuevos', 'InitialConfigController@newUsers');
+Route::middleware('auth:api')->get('v1/configuraciones/changeStatus/{id}', 'InitialConfigController@changeStatus');
