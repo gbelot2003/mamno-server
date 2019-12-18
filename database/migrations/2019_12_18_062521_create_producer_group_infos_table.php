0ceea53646bd3f462f39852be2ec4f11bb07d352
Gerardo Belot <gbelot2003@hotmail.com>
2019-12-18 00:28:35 -0600

    agregando modelos y migraciones restantes

---
+++ database/migrations/2019_12_18_062521_create_producer_group_infos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProducerGroupInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('producer_group_infos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('producer_group_infos');
+    }
+}
